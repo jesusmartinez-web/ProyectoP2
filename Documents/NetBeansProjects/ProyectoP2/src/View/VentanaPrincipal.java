0184ea8844478ebe6ab461a621547b396ddfdcc5
jesusmartinez-web <jesus.martinez@fiuni.edu.py>
2026-06-01 16:33:55 -0300

    Prueba

--- Documents/NetBeansProjects/ProyectoP2/src/View/VentanaPrincipal.java
+++ Documents/NetBeansProjects/ProyectoP2/src/View/VentanaPrincipal.java
@@ -174,6 +174,8 @@ public class VentanaPrincipal extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
+        System.out.println("Prueba");
+        
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -190,7 +192,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
             logger.log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-
+        
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> new VentanaPrincipal().setVisible(true));
     }
